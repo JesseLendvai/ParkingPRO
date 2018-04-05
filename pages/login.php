b54f490d3c5af30a353afa643df4255b61afea01
Rickrom <teamrickrom@gmail.com>
2018-04-05 12:55:52 +0200

    jawuhl

--- pages/login.php
+++ pages/login.php
@@ -35,7 +35,7 @@
                 $_SESSION['Logged_garagemedewerker'] = TRUE;
             } elseif ($SESSION['rol'] == "m") {
                 $_SESSION['Logged_garagemanager'] = TRUE;
-
+            }
             var_dump($_SESSION);
             echo "moker gay ";
 
@@ -204,8 +204,7 @@
         ?>
     </div>
 </div>
-    <footer>
-    </footer>
+    
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
